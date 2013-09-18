d88bb31dcd322d5e4e19cdfca1f1242dea7699b7
MikWil <mikaelwilkon@gmail.com>
2013-09-18 11:09:36 +0200

    NewEngine and stuff

--- BilApp/Program.cs
+++ BilApp/Program.cs
@@ -8,31 +8,45 @@ class Bil
     public string color { get; set; }
     public int Currentspeed { get; set; }
     public int HorsePower { get; set;}
-
+    public Engine Engine;
 
 
     public void Accelerate(int Currentspeed)
     {
-        Currentspeed++;
+        this.Currentspeed += Currentspeed;
 
         Console.WriteLine("You are speeding up to: " + Currentspeed);
     }
-    public void Decelerate(int Currnetspeed)
+    public void Decelerate(int Currentspeed)
     {
-        Currnetspeed++;
-        Console.WriteLine("You are slowing down to: " + Currnetspeed);
+        this.Currentspeed+= Currentspeed;
+        Console.WriteLine("You are slowing down to: " + Currentspeed);
     }
+}
+class Engine
+{
+    public int HorsePower { get; set; }
+    public int FuelCons{ get; set; }
+    public string EngineName { get; set; }
+    public int MakeYear { get; set; }
+
+
 }
 
 class Program
 {
     static void Main()
     {
-        Bil MinBil = new Bil { color = "red", HorsePower = 240, wheel = 4, Currentspeed = 0};
-
-        MinBil.Accelerate(0);
-        Console.WriteLine(MinBil.Currentspeed);
-
+        
+        Engine V6 = new Engine{HorsePower = 200, EngineName = "V6", FuelCons = 1, MakeYear = 2013};
+        Bil MinBil = new Bil { color = "red",Engine = V6, wheel = 4, Currentspeed = 0};
+        
+        MinBil.Accelerate(5);
+        
+       
+        
+        Console.WriteLine("Your speed is: " + MinBil.Currentspeed);
+        Console.WriteLine(MinBil.Engine.EngineName);
     }
 }
 
