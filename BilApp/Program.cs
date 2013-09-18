b6c07c73e00950c9c0fcd34a5efd5d492e9fe9f9
MikWil <mikaelwilkon@gmail.com>
2013-09-18 09:37:34 +0200

    This is the first Draft blahblahas

---
+++ BilApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BilApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("YOLO");
+            Console.WriteLine("YOLO");
+            Console.WriteLine("YOLO");
+
+
+            Console.WriteLine("YOLO");
+            Console.WriteLine("YOLO"); Console.WriteLine("YOLO");
+
+        }
+    }
+}
