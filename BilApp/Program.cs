132432e95db84190d953e838974e16db62fcb156
MikWil <mikaelwilkon@gmail.com>
2013-09-18 10:40:16 +0200

    Added some methods for CurrentSpeed

--- BilApp/Program.cs
+++ BilApp/Program.cs
@@ -6,16 +6,29 @@ class Bil
     public int wheel { get; set; }
     public string[] Maker = new string[3] { "Volvo", "Ford", "BMW" };
     public string color { get; set; }
-    public int speed { get; set; }
+    public int Currentspeed { get; set; }
+    public int HorsePower { get; set;}
 
 
+
+    public void Accelerate(int Currentspeed)
+    {
+        Currentspeed++;
+
+        Console.WriteLine("You are speeding up to: " + Currentspeed);
+    }
+    public void Decelerate(int Currnetspeed)
+    {
+        Currnetspeed++;
+        Console.WriteLine("You are slowing down to: " + Currnetspeed);
+    }
 }
 
 class Program
 {
     static void Main()
     {
-        Bil MinBil = new Bil { color = "red", speed = 240, wheel = 4 };
+        Bil MinBil = new Bil { color = "red", HorsePower = 240, wheel = 4, Currentspeed = 0};
 
         
 
